3fd173dc012c9de6bce4e59cc545ce64a64c2e6a
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-18 20:54:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/mern-crud-client/',
   plugins: [react()],
-  base: '/mern-crud-client/',
 });
