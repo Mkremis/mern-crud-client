8db4e5473d5fe6139ab7b32a57c2d37c0d4ba9a2
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-18 15:40:37 -0300

    1 file modified, 4 added

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { AuthProvider } from './context/AuthContext';
+import TasksPage from './pages/TasksPage';
+import TaskFormPage from './pages/TaskFormPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Tasks Page</h1>} />
-          <Route path="/add-task" element={<h1>New Task</h1>} />
-          <Route path="/tasks/:id" element={<h1>Update Task</h1>} />
-          <Route path="/profile" element={<h1>User Profile</h1>} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/add-task" element={<TaskFormPage/>} />
+          <Route path="/tasks/:id" element={<TaskFormPage/>>} />
+          <Route path="/profile" element={<ProfilePage/>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
